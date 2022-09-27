49d550a8e5e8cb4e165032e9890cea9a3553660b
Lauren Hunter <laurenbhunter00@gmail.com>
2022-09-27 13:19:19 +0200

    photos

--- fetch-photos/src/App.js
+++ fetch-photos/src/App.js
@@ -11,6 +11,20 @@ class App extends Component {
 
 componentDidMount () {
 fetch ('https://wsi001.api.preci.cloud/api/WholeSlideImages/a9d995db-4f0c-45f5-b2d5-846e15918d3a/MetaData')
+.then (response => {
+  console.log('response', response);
+  if (!response.ok) {
+    throw Error("Error fetching the slides");
+  }
+  return response.json()
+  .then(allData => {
+    this.setState({ photos: allData});
+      })
+  .catch(err => {
+    throw Error(err.message);
+     });
+    }
+  );
 }
 
 render () {
