2d159fcef1c943a4cfebb5a3c271b9e69b23d7b7
Lauren Hunter <laurenbhunter00@gmail.com>
2022-09-27 16:36:00 +0200

    dog imagesthrough API

--- fetch-photos/src/App.js
+++ fetch-photos/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
+import PhotoContainer from "./PhotoContainer";
 
 class App extends Component {
   constructor () {
@@ -10,9 +11,8 @@ class App extends Component {
 }
 
 componentDidMount () {
-fetch ('https://wsi001.api.preci.cloud/api/WholeSlideImages/a9d995db-4f0c-45f5-b2d5-846e15918d3a/MetaData')
+fetch ("https://api.thedogapi.com/v1/images/search?limit=10")
 .then (response => {
-  console.log('response', response);
   if (!response.ok) {
     throw Error("Error fetching the slides");
   }
@@ -30,8 +30,34 @@ fetch ('https://wsi001.api.preci.cloud/api/WholeSlideImages/a9d995db-4f0c-45f5-b
 render () {
     return (
       <section className="app">
-        <p>Is this working?</p>
-      </section>
+         <div>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <link href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,300;0,500;0,600;0,700;1,300;1,500;1,600;1,700&display=swap" rel="stylesheet" />
+        <link href="css/styles.css" rel="stylesheet" />
+        <header className="masthead">
+          <div className="container px-5">
+            <div className="row gx-5 align-items-center">
+              <div className="col-lg-6">
+                <div className="mb-5 mb-lg-0 text-center text-lg-start">
+                  <h1 className="display-1 lh-1 mb-3">digital microscopy by PreciPoint</h1>
+                  <h3>Solutions  •   Support   •   Know How</h3>
+                </div>
+                <div className="col-lg-6">
+                </div>
+              </div>
+            </div>
+          </div>
+        </header>
+        <section id="features">
+              <h2 className="display-4 lh-1 mb-4"> Microscopic slide images</h2>
+        </section>
+        <div id="img-wrapper">
+        <PhotoContainer photos={this.state.photos} />
+        </div>
+      </div>
+
+</section>
     );
   }
 }
---
+++ fetch-photos/src/Photo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Photo = (props) => {
+
+  return (
+
+    <section>
+          <img src={props.url} alt="slide photo"/>
+    </section>
+  )
+}
+
+export default Photo
